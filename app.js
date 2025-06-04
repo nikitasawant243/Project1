549015c0b8062da6c6e5c3ac55ac04300902a34b
Nikita Sawant <nikitasawant243@gmail.com>
2025-06-04 22:21:39 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/testListings', async (req, res) => {
         image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price: 1500,
         location: "Bali",
-        country: "Indonesia"
+        country: "Switzerland",
     });
 
     await sampleListing.save();
