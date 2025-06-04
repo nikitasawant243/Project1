81fdade19857faf7bd7e9ffa96144a81e13aa558
Nikita Sawant <nikitasawant243@gmail.com>
2025-06-04 23:42:42 +0530

    Updated files

--- models/listing.js
+++ models/listing.js
@@ -8,15 +8,21 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set:(v) => v === "" ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-        : v,
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e...",
+            set: (v) =>
+                v === ""
+                    ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e..."
+                    : v,
+        }
     },
     price: Number,
     location: String,
     country: String,
 });
+
     
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
