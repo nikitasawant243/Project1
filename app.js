1a2baddc1b835b1466368a3303903e2eb6a7fe2d
Nikita Sawant <nikitasawant243@gmail.com>
2025-06-06 09:51:37 +0530

    new updates files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// Imports and Setup
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
+const path = require("path");
 
+// MongoDB Connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
   console.log('Connected to MongoDB');
@@ -14,25 +17,45 @@ async function main (){
     await mongoose.connect(MONGO_URL);
 }
 
+// View Engine Setup (EJS)
+// Looks for .ejs files in the /views folder inside your project.
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+
+// Home Route
 app.get('/', (req, res) => {
   res.send('Hello World!!'); 
 });
 
-app.get('/testListings', async (req, res) => {
-    let sampleListing = new Listing({
-        title : "My New Villa",
-        description: "By the beach",
-        image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        price: 1500,
-        location: "Bali",
-        country: "Switzerland",
-    });
-
-    await sampleListing.save();
-    console.log('Listing saved:');
-    res.send('Listing saved successfully!');
-
+// Listings Route (Fetch and Render from MongoDB)
+app.get('/listings', async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", {allListings});
 });
+
+// Show Route
+app.get("/listings/:id", async(req, res) => {
+  let {id} = req.params;
+  const listing = await Listing.findById(id); 
+  res.render("listings/show", {listing});
+})
+
+// app.get('/testListings', async (req, res) => {
+//     let sampleListing = new Listing({
+//         title : "My New Villa",
+//         description: "By the beach",
+//         image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         price: 1500,
+//         location: "Bali",
+//         country: "Switzerland",
+//     });
+
+//     await sampleListing.save();
+//     console.log('Listing saved:');
+//     res.send('Listing saved successfully!');
+
+// });
 
 
 app.listen(3000, () => {
