5564b3732eb9e7a86c5c87c3cc4d39063669b9bf
Nikita Sawant <nikitasawant243@gmail.com>
2025-06-14 12:22:32 +0530

    Updated files

--- routes/listing.js
+++ routes/listing.js
@@ -2,82 +2,93 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema} = require("../schema.js");
-const Listing = require('../models/listing.js'); 
+const { listingSchema } = require("../schema.js");
+const Listing = require('../models/listing.js');
 
 const validateListing = (req, res, next) => {
-  let {error} = listingSchema.validate(req.body);
+  let { error } = listingSchema.validate(req.body);
   console.log(error);
-  if(error){
+  if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400,error);
-  }else{
+    throw new ExpressError(400, error);
+  } else {
     next();
   }
 }
 
 // Listings Route (Fetch and Render from MongoDB)
-router.get('/', wrapAsync (async (req, res) => {
+router.get('/', wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("listings/index", {allListings});
+  res.render("listings/index", { allListings });
 }));
 
 // New Route
-router.get("/new", (req,res) => {
-    res.render("listings/new");
+router.get("/new", (req, res) => {
+  res.render("listings/new");
 });
 
 // Show Route
-router.get("/:id",wrapAsync (async(req, res) => {
-  let {id} = req.params;
-  const listing = await Listing.findById(id).populate("reviews"); 
-  res.render("listings/show", {listing});
-}));
+router.get("/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id).populate("reviews");
+  if(!listing){
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show", { listing });
+})); 
 
 
 // Create Route
 
-router.post("/", wrapAsync (async (req,res, next) => {
-  
+router.post("/", wrapAsync(async (req, res, next) => {
+
   const newListing = new Listing(req.body.listing);
   await newListing.save();
+  req.flash("success", "New Listing reated!");
   res.redirect("/listings");
-  })
+})
 
- 
+
 );
 
 // edit Route
-router.get("/:id/edit", wrapAsync (async (req, res) => {
+router.get("/:id/edit", wrapAsync(async (req, res) => {
   // if(!req.body.listing){
   //   throw new ExpressError(400,"Send valid data for listing");
   // }
-  let {id} = req.params;
-  const listing = await Listing.findById(id); 
-  res.render("listings/edit.ejs", {listing});
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if(!listing){
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+  res.render("listings/edit.ejs", { listing });
 }));
 
 
 // Update Route 
-router.put("/:id", wrapAsync (async (req, res) => {
-  let {id} = req.params;
+router.put("/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
   const updatedData = req.body.listing;
 
-// Optional safety: if image.url is present, assign as nested object
-if (updatedData.image && typeof updatedData.image === 'object' && updatedData.image.url) {
-  updatedData.image = { url: updatedData.image.url };
-}
+  // Optional safety: if image.url is present, assign as nested object
+  if (updatedData.image && typeof updatedData.image === 'object' && updatedData.image.url) {
+    updatedData.image = { url: updatedData.image.url };
+  }
 
-await Listing.findByIdAndUpdate(id, updatedData);
+  await Listing.findByIdAndUpdate(id, updatedData);
+  req.flash("success", "Listing Updated!");
 
   res.redirect(`/listings/${id}`);
 }));
 
 // Delete Route
 router.delete("/:id", async (req, res) => {
-  let {id} = req.params;
+  let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
+  req.flash("success", "Listing Deleted!");
   res.redirect("/listings");
 });
 
